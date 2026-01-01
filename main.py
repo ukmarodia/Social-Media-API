72e467256383997c2f90952d3368ff8c6ccd525f
Umang <ukmarodia@gmail.com>
2026-01-02 00:04:21 +0530

    Optional

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from typing import Optional
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str 
+    content:str
+    published: bool = True
+    rating: Optional[int] = None 
 
 @app.get("/")
 async def root():
@@ -10,6 +18,6 @@ def get_post():
     return {"Data": "this is your posts"}
 
 @app.post("/createpost")
-def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title {payload['title']} content:{payload['content']} "}
+def create_post(new_post: Post):
+    print(new_post.rating)
+    return {"data":"new Post"}
