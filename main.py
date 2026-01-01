da9f87356eb1807fcd24e7c646b960beb9192b12
Umang <ukmarodia@gmail.com>
2026-01-01 12:09:20 +0530

    post method

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello WOrld"}
+
+@app.get("/getPost")
+def get_post():
+    return {"Data": "this is your posts"}
+
+@app.post("/createpost")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title {payload['title']} content:{payload['content']} "}
