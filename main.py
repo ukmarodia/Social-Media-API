d0e4d1a78f76d3e01565fb154995ed49916bacf5
Umang <ukmarodia@gmail.com>
2026-01-03 19:21:39 +0530

    Get Single post

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ class Post(BaseModel):
 
 my_posts = [{"title":"title of post1", "content":"content of post1", "id": 1},{"title": "Favourite foods", "content":"I like pizza", "id":2}]
 
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
 @app.get("/")
 async def root():
     return {"message": "Hello WOrld"}
@@ -26,3 +31,9 @@ def create_post(new_post: Post):
     post_dict['id'] = randrange(0,1000000)
     my_posts.append(post_dict)
     return {"data":post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    return {"post_detail":post}
